09380e175bb15c1d51d9942721d507f09f2eb32b
Mario Beltrán <me@mario.dev>
2025-09-18 07:37:17 +0200

    Fix react query devtools setup
    Signed-off-by: Mario Beltrán <me@mario.dev>

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -3,6 +3,7 @@
 import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import customTheme from '@/custom-theme'
 import { queryClient } from '@/query-client'
@@ -15,6 +16,7 @@ const Providers: FC<{ children: ReactNode }> = ({ children }) => {
 			<ChakraProvider theme={customTheme}>
 				<QueryClientProvider client={queryClient}>
 					{children}
+					<ReactQueryDevtools initialIsOpen={false} />
 				</QueryClientProvider>
 			</ChakraProvider>
 		</CacheProvider>
