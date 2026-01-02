35693a532ef3e3db0ec3c2811e3591f34ec96071
Mario Beltrán <me@mario.dev>
2026-01-02 16:51:30 +0100

    Migrate homepage E2E tests
    Signed-off-by: Mario Beltrán <me@mario.dev>

---
+++ e2e/home.spec.ts
@@ -0,0 +1,59 @@
+import { test, expect } from '@playwright/test'
+
+test('should display the corresponding information', async ({ page }) => {
+	await page.goto('/')
+
+	await expect(page).toHaveTitle('Octochangelog')
+
+	const metaDescription = page.locator('meta[name="description"]')
+	await expect(metaDescription).toHaveAttribute(
+		'content',
+		'Compare GitHub changelogs across multiple releases in a single view',
+	)
+
+	await expect(
+		page.getByRole('heading', { level: 1, name: 'Octochangelog' }),
+	).toBeVisible()
+
+	await expect(
+		page.getByRole('heading', {
+			level: 2,
+			name: 'Compare GitHub changelogs in a single view.',
+		}),
+	).toBeVisible()
+
+	const headings = [
+		'Compare releases easily',
+		'Share changelogs',
+		"Don't miss breaking changes",
+		'No manual sorting',
+		'Changes per version',
+	]
+
+	for (const heading of headings) {
+		await expect(
+			page.getByRole('heading', { level: 3, name: heading }),
+		).toBeVisible()
+	}
+
+	await expect(
+		page.getByRole('contentinfo').getByText(/Created.+ by Mario/),
+	).toBeVisible()
+
+	await expect(page.getByRole('link', { name: /mario/i })).toHaveAttribute(
+		'href',
+		'https://mario.dev/',
+	)
+
+	await expect(
+		page.getByRole('link', {
+			name: /octochangelog repository on github/i,
+		}),
+	).toHaveAttribute('href', 'https://github.com/Belco90/octochangelog')
+
+	// TODO happo screenshot
+
+	await page.getByRole('link', { name: 'Try me now!' }).click()
+
+	await expect(page).toHaveURL('/compare')
+})
