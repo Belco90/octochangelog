672d060f3549f346648fdf847ceb9b7a84190f92
Mario Beltrán <me@mario.dev>
2026-01-12 10:22:36 +0100

    Move config to js file

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+// @ts-check
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const prettierConfig = {
+	singleQuote: true,
+	semi: false,
+	useTabs: true,
+}
+
+export default prettierConfig
