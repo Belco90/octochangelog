cd38e9a1faf87a57aa0bc26b3ed3661d3b5a40a3
Mario Beltrán <me@mario.dev>
2026-01-15 13:00:16 +0100

    Improve custom link component

--- src/components/RouteLink.tsx
+++ src/components/RouteLink.tsx
@@ -1,16 +1,30 @@
 import { Link as ChakraLink } from '@chakra-ui/react'
-import { Link as TanStackLink } from '@tanstack/react-router'
+import { createLink } from '@tanstack/react-router'
 
-import type { LinkProps as ChakraLinkProps } from '@chakra-ui/react'
-import type { LinkProps as TanStackLinkProps } from '@tanstack/react-router'
+import type { LinkComponent } from '@tanstack/react-router'
+import type { ComponentPropsWithoutRef, RefObject } from 'react'
 
-type RouteLinkProps = Omit<ChakraLinkProps, 'href' | 'as'> &
-	Omit<TanStackLinkProps, 'href'>
+type CustomChakraLinkProps = Omit<
+	ComponentPropsWithoutRef<typeof ChakraLink>,
+	'href' | 'as'
+>
 
-export function RouteLink({ children, to, ...remainingProps }: RouteLinkProps) {
-	return (
-		<ChakraLink as={TanStackLink} href={to} {...remainingProps}>
-			{children}
-		</ChakraLink>
-	)
+const ChakraLinkComponent = ({
+	ref,
+	...props
+}: CustomChakraLinkProps & { ref?: RefObject<HTMLAnchorElement | null> }) => {
+	return <ChakraLink ref={ref} {...props} />
+}
+
+const CreatedLinkComponent = createLink(ChakraLinkComponent)
+
+/**
+ * This component takes the same properties as the Chakra UI Link component,
+ * enabling seamless integration with Chakra's styling.
+ *
+ * This is the recommended way of creating custom links with TanStack Router:
+ * https://tanstack.com/router/latest/docs/framework/react/guide/custom-link#chakra-ui-example*
+ */
+export const RouteLink: LinkComponent<typeof ChakraLinkComponent> = (props) => {
+	return <CreatedLinkComponent {...props} />
 }
