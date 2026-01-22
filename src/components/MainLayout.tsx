3c4ae15ba61edfb8ff2b731f4e76d15b4a174d37
Mario Beltrán <me@mario.dev>
2026-01-22 12:49:34 +0100

    Migrate main layout

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,19 +1,17 @@
-import { Flex } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
 
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
 import type { PropsWithRequiredChildren } from '@/models'
 
-// function OldMainLayout({ children }: PropsWithRequiredChildren) {
-// 	return (
-// 		<Flex height="100%" direction="column">
-// 			<Header />
-// 			<Box as="main" flex="1 0 auto" bgColor="background2">
-// 				{children}
-// 			</Box>
-// 			<Footer />
-// 		</Flex>
-// 	)
-// }
-
 export function MainLayout({ children }: PropsWithRequiredChildren) {
-	return <Flex height="full">{children}</Flex>
+	return (
+		<Flex height="full" direction="column">
+			<Header />
+			<Box as="main" flex="1 0 auto">
+				{children}
+			</Box>
+			<Footer />
+		</Flex>
+	)
 }
