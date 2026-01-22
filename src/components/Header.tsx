aa5aa25bbd07400e8ab7ae04e04ede817086b239
Mario Beltrán <me@mario.dev>
2026-01-22 16:04:14 +0100

    Adjust header padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,7 @@ export const Header = (props: BoxProps) => {
 			borderBottomWidth={1}
 			borderColor="brand.emphasized"
 		>
-			<Box p={2}>
+			<Box px={2} py={1}>
 				<Flex justify="space-between" alignItems="center">
 					<Flex align="center">
 						<RouteLink
